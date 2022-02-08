0634ec211cba4779e6ae0f18f8a33b653a1b16f5
karnet <e.abramovicj@icode.by>
2022-02-08 20:10:42 +0100

    refactored

---
+++ stress-testing/common_settings.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+
+
+class PostgresSetting(BaseSettings):
+    NAME: str = 'postgres'
+    USER: str = 'postgres'
+    PASSWORD: str = 'postgres'
+    HOST: str = 'localhost'
+    PORT: int = 6432
+
+    class Config:
+        env_prefix = "DB_"
+
+
+class CommonSettings(BaseSettings):
+    POSTGRES: PostgresSetting = PostgresSetting()
